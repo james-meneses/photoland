52ca0ddcf4f5a56b2668077bd0a7fa2579adb24b
James Bond <jhemerson.comercial@gmail.com>
2016-02-18 16:46:55 -0200

    Learning AngularJS - Forms Pt. 1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,34 +1,9 @@
 // require('angular')
 (function(){
+	require('./controllers/MainController')
 
 	var app = angular.module('app', []);
 
-	app.factory('messages', function(){
-		var messages = {};
-
-		messages.list = [];
-		messages.add = function(msg) {
-			messages.list.push({id: messages.list.length, text: msg});
-		};
-
-		return messages;
-	});
-
-	app.controller('ListCtrl', function (messages) {
-		var self = this;
-
-		self.messages = messages.list;
-	});
-
-	app.controller('PostCtrl', function (messages) {
-		var self = this;
-		self.newMessage = 'You know nothing, Jon Snow';
-		
-		self.addMessage = function(message) {
-			messages.add(message);
-			self.newMessage = '';
-		};
-
-	});
+	app.controller('MainController', MainController);
 
 }) ();
