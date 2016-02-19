86120adc9753dc3c3ff91836995bf02f187b9341
James Bond <jhemerson.comercial@gmail.com>
2016-02-19 02:24:54 -0200

    Learning AngularJS - Forms with Directives

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,4 +6,58 @@
 
 	app.controller('FormController', ['$scope', FormController]);
 
+	var INTEGER_REGEXP = /^\-?\d+$/;
+
+	app.directive('integer', function() {
+		return {
+			require: 'ngModel',
+			link: function(scope, elm, attrs, ctrl) {
+				ctrl.$validators.integer = function(modelValue, viewValue) {
+					if(ctrl.$isEmpty(modelValue)){
+						// consider empty models to be valid
+						return true;
+					}
+
+					if(INTEGER_REGEXP.test(viewValue)) {
+						// it's valid
+						return true;
+					}
+
+					//if dindn't passed already, for sure, it's invalid
+					return false;
+				};
+			}
+		};
+	});
+
+	app.directive('username', function($q, $timeout) {
+		return {
+			require: 'ngModel',
+			link: function(scope, elm, attrs, ctrl) {
+				var usernames = ['Jim', 'James', 'John', 'Jackie'];
+
+				ctrl.$asyncValidators.username = function(modelValue, viewValue) {
+					if(ctrl.$isEmpty(modelValue)) {
+						// consider empty model valid
+						return $q.when();
+					}
+
+					var def = $q.defer();
+
+					$timeout(function() {
+						// Mock a delayed response
+						if (usernames.indexOf(modelValue) === -1) {
+							// The username is available
+							def.resolve();
+						} else {
+							def.reject();
+						}
+					}, 2000);
+
+					return def.promise;
+				};
+			}
+		};
+	});
+
 }) ();
