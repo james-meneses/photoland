f7cddcceb3f2d90090dc209fdb673ac4a22154b1
James Bond <jhemerson.comercial@gmail.com>
2016-02-19 04:24:18 -0200

    Learning AngularJS - Promises Pt. 1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,63 +1,32 @@
 // require('angular')
 (function(){
-	var FormController = require('./controllers/FormController');
 
-	var app = angular.module('app', ['ui.bootstrap']);
+	function getData($timeout, $q) {
+		return function() {
+			return $q(function(resolve, reject){
+				$timeout(function(){
+					resolve(Math.floor(Math.random() * 10))
+				}, 2000)
+			})
+		}
+	}
 
-	app.controller('FormController', ['$scope', FormController]);
+	var app = angular.module('app', []);
 
-	var INTEGER_REGEXP = /^\-?\d+$/;
-
-	app.directive('integer', function() {
-		return {
-			require: 'ngModel',
-			link: function(scope, elm, attrs, ctrl) {
-				ctrl.$validators.integer = function(modelValue, viewValue) {
-					if(ctrl.$isEmpty(modelValue)){
-						// consider empty models to be valid
-						return true;
-					}
-
-					if(INTEGER_REGEXP.test(viewValue)) {
-						// it's valid
-						return true;
-					}
-
-					//if dindn't passed already, for sure, it's invalid
-					return false;
-				};
-			}
-		};
-	});
-
-	app.directive('username', function($q, $timeout) {
-		return {
-			require: 'ngModel',
-			link: function(scope, elm, attrs, ctrl) {
-				var usernames = ['Jim', 'James', 'John', 'Jackie'];
-
-				ctrl.$asyncValidators.username = function(modelValue, viewValue) {
-					if(ctrl.$isEmpty(modelValue)) {
-						// consider empty model valid
-						return $q.when();
-					}
-
-					var def = $q.defer();
-
-					$timeout(function() {
-						// Mock a delayed response
-						if (usernames.indexOf(modelValue) === -1) {
-							// The username is available
-							def.resolve();
-						} else {
-							def.reject();
-						}
-					}, 2000);
-
-					return def.promise;
-				};
-			}
-		};
-	});
+	app
+	   .factory('getData', getData)
+	   .run(function(getData){
+	   		var promise = getData()
+	   			.then(function(num) {
+	   				console.log(num)
+	   				return num * 2
+	   			})
+	   			.then(function(num) {
+	   				console.log(num)
+	   			})
+	   			.finally(function() {
+	   				console.log('Finished at ', new Date())
+	   			});
+	   })
 
 }) ();
