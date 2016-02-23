33ffcc270890522fbd3b366d742b50198edba4f8
James Bond <jhemerson.comercial@gmail.com>
2016-02-23 16:52:23 -0300

    Learning AngularJS - Directives (Directives talking to Controllers using ()) Pt.2.2.2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,14 +21,18 @@
 	.directive('entering', function() {
 		return function(scope, element, attrs) {
 			element.bind('mouseenter', function() {
-				scope.fun.start()
+				// It's better that the directive don't know much about the directive. Keep them separated
+				// Avoid doing smth like: scope.fun.start()
+				// Instead use scope.$apply(), like so
+				scope.$apply(attrs.entering)
 			})
 		}
 	})
 	.directive('leaving', function() {
 		return function(scope, element, attrs){
 			element.bind('mouseleave', function() {
-				scope.fun.end()
+				// As in the entering event, here we do the same, just changing the method to be called
+				scope.$apply(attrs.leaving)
 			})
 		}
 	})
