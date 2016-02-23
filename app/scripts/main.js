1a171194a57fb13b19782e103f54e5dcc17b3b4e
James Bond <jhemerson.comercial@gmail.com>
2016-02-23 05:08:18 -0300

    Learning AngularJS - Filters Pt.2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,27 +3,24 @@
 
 	function TestCtrl() {
 		var self = this;
-		self.myString = "Hello world";
-		self.email = "";
-	}
+		self.people  = [
+				{ name: 'Valerie', species: 'bitch' },
+				{ name: 'Nanda',   species: 'fox' },
+				{ name: 'Lia',     species: 'cat' },
+				{ name: 'Nath',    species: 'bitch' },
+				{ name: 'Camile',  species: 'tiger' },
+				{ name: 'Tiffany', species: 'cat' },
+				{ name: 'Gaby',    species: 'bitch' },
+				{ name: 'Key',     species: 'fox' }
+			];
 
-	function CapitalizeFilter() {
-		return function (text) {
-			return text.toUpperCase() ;
-		}
-	}
-
-	function IsValidEmail() {
-		return function(email) {
-			return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(email) ? "Válido" : "Inválido";
+		self.addPerson = function (name, species) {
+			self.people.push({ name: name, species: species});
 		}
 	}
 
 	var app = angular.module('app', []);
 
 	app.controller('TestCtrl', TestCtrl);
-
-	app.filter('capitalize', CapitalizeFilter);
-	app.filter('validEmail', IsValidEmail);
 
 }) ();
