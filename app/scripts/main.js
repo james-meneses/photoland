5feec19ce3f3242ee0bea2d2e36415907165c503
James Bond <jhemerson.comercial@gmail.com>
2016-02-23 15:22:12 -0300

    Learning AngularJS - Directives Pt.1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,22 @@
 // require('angular')
 (function(){
 
-	function TestCtrl() {
-		var self = this;
-		self.people  = [
-				{ name: 'Valerie', species: 'bitch' },
-				{ name: 'Nanda',   species: 'fox' },
-				{ name: 'Lia',     species: 'cat' },
-				{ name: 'Nath',    species: 'bitch' },
-				{ name: 'Camile',  species: 'tiger' },
-				{ name: 'Tiffany', species: 'cat' },
-				{ name: 'Gaby',    species: 'bitch' },
-				{ name: 'Key',     species: 'fox' }
-			];
-
-		self.addPerson = function (name, species) {
-			self.people.push({ name: name, species: species});
-		}
-	}
-
 	var app = angular.module('app', []);
 
-	app.controller('TestCtrl', TestCtrl);
+	app.directive("welcome", function() {
+		return {
+			restrict: "A",
+			link: function() {
+				alert("Hey there!");
+			}
+		}
+	}).directive("goodbye", function() {
+		return {
+			restrict: "A",
+			link: function() {
+				alert("Hora de dizer adeus!");
+			}
+		}
+	})
 
 }) ();
