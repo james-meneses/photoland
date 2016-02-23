0d202754e9fcfb24125541d74f7d20ff24677b53
James Bond <jhemerson.comercial@gmail.com>
2016-02-23 20:51:36 -0300

    Learning AngularJS - Directives (Transclude) Pt.2.4

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,59 +3,12 @@
 
 	var app = angular.module('app', []);
 
-	app
-	.directive('welcome', function() {
-		return {
+	app.directive('welcome', function() {
+		return { 
 			restrict: 'E',
-			// Below we isolate the scope, so that each directive instance has its own local scope
 			scope: {},
-			controller: function($scope) {
-				var self = this;
-				$scope.words = [];
-
-				self.greetBitch = function() {
-					$scope.words.push("Hey bitch, I came to slay")
-				}
-				self.greetLady = function () {
-					$scope.words.push("Hey pretty little lady")
-				}
-				self.greetLonelyGirl = function() {
-					$scope.words.push("Hey there lonely girl")
-				}
-			},
-			link: function(scope, element){
-				element.bind('mouseenter', function(){
-					console.log(scope.words);
-				})
-			}
-		}
-	})
-
-	app.directive('lady', function(){
-		return {
-			require: 'welcome',
-			link: function(scope, element, attrs, welcomeCtrl) {
-				welcomeCtrl.greetLady();
-			}
-		}
-	})
-
-
-	app.directive('bitch', function() {
-		return {
-			require: 'welcome',
-			link: function(scope, element, attrs, welcomeCtrl) {
-				welcomeCtrl.greetBitch();
-			}
-		}
-	})
-
-	app.directive("lonelygirl", function() {
-		return {
-			require: 'welcome',
-			link: function(scope, element, attrs, welcomeCtrl) {
-				welcomeCtrl.greetLonelyGirl();
-			}
+			transclude: true,
+			template: '<div>This is what I live for</div><ng-transclude></ng-transclude>'
 		}
 	})
 
