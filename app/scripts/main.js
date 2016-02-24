9b5ffaa414dcd6d3b7875cb95298fcc2ee538f08
James Bond <jhemerson.comercial@gmail.com>
2016-02-24 20:51:03 -0300

    Learning AngularJS - Creating a Tab System Pt.1.1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,61 @@
-// require('angular')
-(function(){
+;(function(window) {
 
-	var app = angular.module('app', []);
+	var app = angular.module('app', [])
 
-	app.directive('welcome', function() {
-		return { 
+	app.directive('tab', function() {
+		return{
 			restrict: 'E',
-			scope: {},
-			transclude: true,
-			template: '<div>This is what I live for</div><ng-transclude></ng-transclude>'
+			transclude: 'true',
+			template: '<div role="tabpanel" class="alone" ng-show="active" ng-transclude></div>',
+			require: '^tabset',
+			scope: { 
+				heading: '@'
+			},
+			link: function(scope, elem, attr, tabsetCtrl) {
+				scope.active = false
+
+				scope.disabled = false
+
+				if( attr.disable ) {
+					attr.$observe('disable', function(value) {
+						scope.disabled = ( value === 'true' )
+					})
+				}
+
+				tabsetCtrl.addTab(scope)
+			}
 		}
 	})
+	.directive('tabset', function() {
+		return {
+			restrict: 'E',
+			transclude: true,
+			scope: { },
+			templateUrl: 'tabset.html',
+			bindToController: true,
+			controllerAs: 'tabset',
+			controller: function() {
+				var self  = this
+				self.tabs = []
 
-}) ();
+				self.addTab = function addTab(tab) {
+					self.tabs.push(tab)
+
+					tab.active = self.tabs.length === 1
+				}
+
+				self.select = function (selectedTab) {
+					if( selectedTab.disabled ) return
+					
+					self.tabs.forEach(function(el) {
+						el.active = false
+					})
+
+					selectedTab.active = true
+				}
+
+			}
+		}
+	});
+
+})(window);
